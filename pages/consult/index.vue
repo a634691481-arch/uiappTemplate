a3cf681f5b0a12c8e4c4bbeb9bfaf8c15e4e6d24
yangliu <634691481@qq.com>
2026-04-30 10:53:57 +0800

    refactor: consult 首页移除冗余 activeColor 属性传递

--- pages/consult/index.vue
+++ pages/consult/index.vue
@@ -34,7 +34,7 @@
       </view>
     </view>
 
-    <yy-tip-modal v-model="showExplain" title="咨询服务说明" :list="explainList" :active-color="pagingConfig.color" />
+    <yy-tip-modal v-model="showExplain" title="咨询服务说明" :list="explainList" />
   </yy-paging>
 </template>
 
