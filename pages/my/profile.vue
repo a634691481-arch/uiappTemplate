ee4d1e089c13b80cc5c0bf6432e88f85da68f556
yangliu <634691481@qq.com>
2026-04-30 10:30:16 +0800

    feat: 个人资料页面开启下拉刷新

--- pages/my/profile.vue
+++ pages/my/profile.vue
@@ -63,8 +63,8 @@
 
   const pagingConfig = ref({
     auto: false,
-    refresherEnabled: false,
-    showRefresherWhenReload: false,
+    refresherEnabled: true,
+    showRefresherWhenReload: true,
     showTabbar: false,
     hideNav: false,
     showNavBack: true,
@@ -90,6 +90,7 @@
       { label: '个性签名', icon: 'ri:edit-line', value: '探索世界，发现美好', field: 'signature' },
       { label: '主题', icon: 'ri:palette-line', value: '默认主题', field: 'theme' },
       // { label: '深色模式', icon: 'ri:moon-line', value: '关闭', field: 'darkMode' },
+      // { label: '清除缓存', icon: 'ri:delete-bin-line', value: '0.00MB', field: 'clearCache' },
     ],
   })
 
